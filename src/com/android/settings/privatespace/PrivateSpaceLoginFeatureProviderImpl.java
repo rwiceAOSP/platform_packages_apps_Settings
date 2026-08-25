6d70fd20035a3bf7da6f709db979243f3f5dfad7
rwicept <thanhtrung08022010@gmail.com>
2026-08-25 08:59:49 +0700

    Settings: Initiate account login during Private Space setup
    Change-Id: Id9852a335e2ea1d7e5c6823dae272b330230fcfc

--- src/com/android/settings/privatespace/PrivateSpaceLoginFeatureProviderImpl.java
+++ src/com/android/settings/privatespace/PrivateSpaceLoginFeatureProviderImpl.java
@@ -16,17 +16,60 @@
 
 package com.android.settings.privatespace;
 
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 
-/** Stub class for Private space to not initiate account login during setup */
+import com.android.settings.R;
+import com.android.settings.privatespace.PrivateSpaceLoginFeatureProvider;
+
+import java.io.IOException;
+
+/** Java class for Private space to initiate account login during setup */
 public class PrivateSpaceLoginFeatureProviderImpl implements PrivateSpaceLoginFeatureProvider {
+
+    private static final String TAG = "PrivateSpaceGoogleImpl";
+
+    private ActivityResultLauncher<Intent> mAddAccountToPrivateProfile;
+
+    private final AccountManagerCallback<Bundle> mCallbackLocal =
+            new AccountManagerCallback<>() {
+                @Override
+                public void run(AccountManagerFuture<Bundle> accountManagerFuture) {
+                    try {
+                        final Intent intent =
+                                accountManagerFuture
+                                        .getResult()
+                                        .getParcelable(AccountManager.KEY_INTENT);
+                        if (intent == null) {
+                            Log.e(TAG, "Failed to retrieve add account intent from authenticator");
+                        } else {
+                            mAddAccountToPrivateProfile.launch(intent);
+                        }
+                    } catch (AuthenticatorException | OperationCanceledException | IOException e) {
+                        Log.e(TAG, "Failed to get add account intent Activity:  ", e);
+                    }
+                }
+            };
+
     @Override
     public boolean initiateAccountLogin(@NonNull Context context,
             @NonNull ActivityResultLauncher<Intent> resultLauncher) {
-        return false;
+        mAddAccountToPrivateProfile = resultLauncher;
+        final Bundle addAccountOptions = new Bundle();
+        addAccountOptions.putBoolean("allow_skip", true);
+        AccountManager.get(context)
+                .addAccount(
+                        "com.google", null, null, addAccountOptions, null, mCallbackLocal, null);
+        return context.getResources().getBoolean(R.bool.config_privatespace_account_login_enabled);
     }
 }
